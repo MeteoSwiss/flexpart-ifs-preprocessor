56a34ade83c5ea7b8bb9a6eff6949ac7cd15b321
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2026-03-11 16:41:34 +0100

    refactor InputDataAggregatorEvent - only extract datetime from correct stream/feed files

--- flexpart_ifs_preprocessor/domain/data_model.py
+++ flexpart_ifs_preprocessor/domain/data_model.py
@@ -30,24 +30,22 @@ class IFSForecastFile:
     def to_dict(self) -> dict[str, typing.Any]:
         return asdict(self)
 
-@dataclass
-class InputDataAggregatorEvent:
-    object_key: str
-    filename: str
-    forecast_ref_time: datetime = field(init=False)
-    step: int = field(init=False)
-    domain: Domain = field(init=False)
-    stream: Stream = field(init=False)
 
-    def __post_init__(self, data: dict) -> None:
+class InputDataAggregatorEvent:
+
+    def __init__(self, data: dict) -> None:
+
         self.object_key = data['objectStoreUuid']
         self.filename = data['fileName']
-
-        self.forecast_ref_time = self._extract_datetime(self.filename)
-        self.step = self.extract_lead_time(self.filename)
         self.stream = self._extract_stream()
         self.domain = self._extract_domain()
 
+        if self.stream != Stream.UNKNOWN:
+            self.forecast_ref_time = self._extract_datetime()
+            self.step = self._extract_lead_time()
+        else:
+            self.forecast_ref_time = None
+            self.step = None
 
     def _extract_stream(self) -> Stream:
         match = re.search(r'(S[456]Y)', self.filename.upper())
@@ -70,10 +68,11 @@ class InputDataAggregatorEvent:
             raise ValueError(f"No datetime found in string: {s}")
         return datetime.strptime(match.group(1), "%Y%m%dT%H%M%SZ").replace(tzinfo=timezone.utc)
 
-    def extract_lead_time(self, s: str) -> int:
+    def _extract_lead_time(self, s: str) -> int:
         match = re.search(r'_(\d+)([a-z]+)$', s)
         if not match:
             raise ValueError(f"No lead time found in string: {s}")
         value, unit = match.group(1), match.group(2)
-        assert unit == "h", f"Expected unit 'h', got '{unit}'"
+        if unit != "h":
+            raise ValueError(f"Expected unit 'h', got {unit}")
         return int(value)
